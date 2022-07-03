5ab838661f157f32aaf57decff0b637e3cb3077a
AndrewRoesch <42554067+AndrewRoesch@users.noreply.github.com>
2022-07-03 14:31:08 -0700

    npm ignore

--- projects/ng2-image-upload/src/SetupPackage.ts
+++ projects/ng2-image-upload/src/SetupPackage.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+const fs = require('fs');
 
 // DO NOT DELETE THIS FILE
 // This file is used by build system to build a clean npm package with the compiled js files in the root of the package.
@@ -9,13 +9,10 @@ function main() {
     const sourceObj = JSON.parse(source);
     sourceObj.scripts = {};
     sourceObj.devDependencies = {};
-    if (sourceObj.main.startsWith("dist/")) {
-        sourceObj.main = sourceObj.main.slice(5);
-    }
     fs.writeFileSync(__dirname + "/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
     fs.writeFileSync(__dirname + "/version.txt", Buffer.from(sourceObj.version, "utf-8") );
 
-    fs.copyFileSync(__dirname + "/../.npmignore", __dirname + "/.npmignore");
+    fs.copyFileSync(__dirname + "/../../.npmignore", __dirname + "/.npmignore");
 }
 
 main();
