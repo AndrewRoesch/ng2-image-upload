f00c3ff2cf679f7afa4e733e1ad8e83336c0db7f
AndrewRoesch <42554067+AndrewRoesch@users.noreply.github.com>
2022-07-03 21:51:35 -0700

    package builds

--- projects/ng2-image-upload/src/SetupPackage.ts
+++ projects/ng2-image-upload/src/SetupPackage.ts
@@ -5,14 +5,18 @@ const fs = require('fs');
 // It will not be included in the npm package.
 
 function main() {
+    const outerPack = fs.readFileSync(__dirname + "/../../../package.json").toString('utf-8');
     const source = fs.readFileSync(__dirname + "/../package.json").toString('utf-8');
     const sourceObj = JSON.parse(source);
+    const outerPackObj = JSON.parse(outerPack);
+
+    sourceObj.version = outerPackObj.version;
     sourceObj.scripts = {};
     sourceObj.devDependencies = {};
-    fs.writeFileSync(__dirname + "/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
-    fs.writeFileSync(__dirname + "/version.txt", Buffer.from(sourceObj.version, "utf-8") );
+    fs.writeFileSync(__dirname + "/../../../dist/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
+    fs.writeFileSync(__dirname + "/../../../dist/version.txt", Buffer.from(outerPackObj.version, "utf-8") );
 
-    fs.copyFileSync(__dirname + "/../../.npmignore", __dirname + "/.npmignore");
+    fs.copyFileSync(__dirname + "/../../.npmignore", __dirname + "/../../../dist/.npmignore");
 }
 
 main();
